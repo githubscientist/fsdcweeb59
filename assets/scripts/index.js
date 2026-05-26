07620a8cd06b03f83aaa9863430dc0ec831b65e1
githubscientist <sathish.datascientist@gmail.com>
2026-05-26 19:48:54 +0530

    append vs appendChild

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,8 +2,6 @@ const header = document.createElement('h2');
 
 header.textContent = 'Welcome to DOM Manipulation!';
 
-document.body.appendChild(header);
-
 // create a new paragraph element
 const paragraph = document.createElement('p');
 
@@ -11,4 +9,4 @@ const paragraph = document.createElement('p');
 paragraph.textContent = "Hello, World!";
 
 // append or insert this element to the html document body
-document.body.appendChild(paragraph);
+document.body.append(header, paragraph);
