365384d0478cd2bc8228c6cc421f5b02129d24bd
githubscientist <sathish.datascientist@gmail.com>
2026-05-26 20:19:26 +0530

    elements selection

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,9 @@
-console.log(document.getElementsByClassName('headerClass'));
+const headers = document.getElementsByTagName('h1');
+
+headers[0].style.color = 'green';
+
+const paragraphs = document.getElementsByTagName('p');
+
+for (let index = 0; index < paragraphs.length; index++){
+    paragraphs[index].style.color = 'red';
+}
