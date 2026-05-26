dde5cadae8e679518a1bcb0514dfbc8b80e52e11
githubscientist <sathish.datascientist@gmail.com>
2026-05-26 19:58:04 +0530

    getElementsByTagName

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,1 @@
-const header = document.createElement('h2');
-
-header.textContent = 'Welcome to DOM Manipulation!';
-
-// create a new paragraph element
-const paragraph = document.createElement('p');
-
-// set the content of the paragraph as "Hello, World!"
-paragraph.textContent = "Hello, World!";
-
-// append or insert this element to the html document body
-document.body.append(header, paragraph);
+console.log(document.getElementsByTagName('h1')[1].textContent);
