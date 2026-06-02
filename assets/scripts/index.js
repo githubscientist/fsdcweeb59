6532b19118e7ae105d14ee7715b86ec701395107
githubscientist <sathish.datascientist@gmail.com>
2026-06-02 21:00:33 +0530

    rendering html based on api response

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,31 @@
+// select the usersContainer
+const usersContainer = document.getElementById('usersContainer');
+
 async function fetchUsers() {
     // we are making a http request using fetch function
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await response.json();
-    console.log(data);
+    
+    if (data.length !== 0) {
+        // clear the existing div block
+        usersContainer.innerHTML = '';
+
+        // we have users
+        // create elements
+        // create an unordered list
+        const ul = document.createElement('ul');
+
+        data
+            .forEach(user => {
+                const li = document.createElement('li');
+                li.textContent = user.name;
+                ul.append(li);
+            })
+            
+        // ul to be appended to usersContainer
+        usersContainer.append(ul);
+    }
 }
 
 fetchUsers();
+
