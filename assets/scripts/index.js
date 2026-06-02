7c3584bbff3f3971917dd2ef0414e091d4fe2099
githubscientist <sathish.datascientist@gmail.com>
2026-06-02 20:52:07 +0530

    async await with fetch api

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,15 +1,8 @@
-// we are making a http request using fetch function
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => {
-        // this executes when the api call succeeds
-        return response.json();
-    }) 
-    .then((data) => {
-        // this block executes after response.json() promise
-        // is either resolved or rejected
-        console.log(data);
-    })
-    .catch(() => {
-        //    this executes when the api call fails
-        console.log('failure');
-   })
+async function fetchUsers() {
+    // we are making a http request using fetch function
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    console.log(data);
+}
+
+fetchUsers();
