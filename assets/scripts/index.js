9e5c2c843d9b60d225a00c2cd643ac3d258d0b05
githubscientist <sathish.datascientist@gmail.com>
2026-06-02 20:45:51 +0530

    fetch api example --revisited

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,15 @@
-fetch('https://jsonplaceholder.typicode.com/users', {
-    method: 'GET'
-})
+// we are making a http request using fetch function
+fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => {
+        // this executes when the api call succeeds
         return response.json();
-    })
+    }) 
     .then((data) => {
+        // this block executes after response.json() promise
+        // is either resolved or rejected
         console.log(data);
     })
     .catch(() => {
-        console.log('api call fails');
-    })
+        //    this executes when the api call fails
+        console.log('failure');
+   })
