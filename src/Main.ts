fb31cd1e31ac90ca0b27ccb1e9460f09b91b4ba0
githubscientist <sathish.datascientist@gmail.com>
2026-06-04 19:51:21 +0530

    tuples in typescript

--- src/Main.ts
+++ src/Main.ts
@@ -1,6 +1,7 @@
-// any type
-let x: any = 10;
+/*
+    Tuple: A tuple is a fixed-length array where each element can have a different type. It allows you to group together values of different types into a single unit. 
+*/
 
-x = 'ten';
+let tuple: [number, number, string, number] = [1, 2, 'hello', 5];
 
-console.log(x);
+console.log(tuple);
