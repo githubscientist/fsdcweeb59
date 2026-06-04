ac8bffd3c5842752be9ea416e5e8fc612f9e64d4
githubscientist <sathish.datascientist@gmail.com>
2026-06-04 18:46:19 +0530

    variable assignment and type annotation in typescript

--- src/Main.ts
+++ src/Main.ts
@@ -1,3 +1,7 @@
-let x: number = 10;
+// declaration statement: we are declaring a variable named 'x' of type 'number'
+let x: number;
+
+// we are assigning a value to the variable 'x'
+x = 10;
 
 console.log('value of x:', x);
