1cb2635177c9e697a34e3bde4236817e3f16a91e
githubscientist <sathish.datascientist@gmail.com>
2026-06-04 18:47:10 +0530

    variable re-assignment throws error if the type mismatches

--- src/Main.ts
+++ src/Main.ts
@@ -1,7 +1,14 @@
-// declaration statement: we are declaring a variable named 'x' of type 'number'
-let x: number;
 
-// we are assigning a value to the variable 'x'
-x = 10;
+// declaration statement with initialization: we are declaring a variable named 'x' of type 'number' and initializing it with the value 10
+// let x: number = 10;
+// console.log('value of x:', x);
+
+// ----------------------------------------------------------
+
+// data type of x is now 'number'
+let x: number = 10;
+
+// we cannot re-assign a value of a different type to 'x'
+x = 'ten'; // Error: Type 'string' is not assignable to type 'number'
 
 console.log('value of x:', x);
