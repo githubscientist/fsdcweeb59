249e6ce6abeb42c4c494a972116bfc9ffa2e4bef
githubscientist <sathish.datascientist@gmail.com>
2026-06-05 20:18:12 +0530

    inline object type annotation

--- src/Main.ts
+++ src/Main.ts
@@ -1,54 +1,10 @@
-/*
-    Interfaces: Interfaces in typescript are used to define the structure of an object.
-
-    - in an object oriented programming language, interfaces are used to define the structure of a class and the methods that a class should implement.
-
-    the methods inside the interface are abstract methods, which means that they do not have a body and they must be implemented by the class that implements the interface.
-
-    this provides data abstraction. data abstraction is the process of hiding the implementation details and showing only the functionality to the user.
-*/
-
-interface IStudent {
+// inline object type annotation
+let student: {
     name: string;
     age: number;
-    place: string;
-
-    getAge(): void;
+} = {
+    name: "Sathish",
+    age: 25
 }
 
-
-class Student implements IStudent {
-    
-    // declare the variables
-    name: string;
-    age: number;
-    place: string;
-    designation: string;
-
-    constructor() {
-        this.name = '';
-        this.age = 0;
-        this.place = '';
-        this.designation = '';
-    }
-
-    getAge(): void {
-        console.log('Age:', this.age);
-    }
-
-    getPlace(): void {
-        console.log('Place:', this.place);
-    }
-
-    getName(): void {
-        console.log('Name:', this.name);
-    }
-}
-
-const s1 = new Student();
-
-s1.name = 'Sathish';
-s1.age = 25;
-s1.place = 'Chennai';
-
-s1.getAge();
+console.log(student.name);
