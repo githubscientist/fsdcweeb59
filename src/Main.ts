1fe25178a2abb47528ffa28b223f89d6f769cab3
githubscientist <sathish.datascientist@gmail.com>
2026-06-05 19:47:46 +0530

    classes, objects, methods in typescript

--- src/Main.ts
+++ src/Main.ts
@@ -1,13 +1,32 @@
-// function to add two numbers
-// optional parameter
-function add(firstNumber: number, secondNumber?: number): number {
-    if (secondNumber) {
-        return firstNumber + secondNumber;
+class Student {
+    
+    // declare the variables
+    name: string;
+    age: number;
+    place: string;
+
+    constructor() {
+        this.name = '';
+        this.age = 0;
+        this.place = '';
     }
 
-    return firstNumber;
+    getAge(): void {
+        console.log('Age:', this.age);
+    }
 }
 
-// due to type inference, automatically inherits the number type
-let result: number = add(5, 6);
-console.log(result);
+// create an object to the class
+let s1 = new Student();
+
+s1.name = 'krish';
+s1.age = 25;
+s1.place = 'chennai';
+
+let s2 = new Student();
+
+s2.name = 'sathish';
+s2.age = 30;
+s2.place = 'coimbatore';
+
+s2.getAge();
